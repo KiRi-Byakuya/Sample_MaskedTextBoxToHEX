462e3636f0637579a00aa66ea783386fa76d854d
KiRi-Byakuya <gt86.brz.sub@gmail.com>
2014-12-18 12:36:05 +0900

    main push

---
+++ Sample_MaskTextBoxToHEX/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Sample_MaskTextBoxToHEX {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HEX";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(15, 28);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(308, 19);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 68);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ Sample_MaskTextBoxToHEX/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample_MaskTextBoxToHEX {
+    public partial class Form1 : Form {
+        
+        public Form1() {
+            InitializeComponent();
+            // タイトル
+            this.Text = "Sample_MaskedTextBoxToHEX";
+            // フォーム 最小・最大化禁止
+            this.MinimizeBox = this.MaximizeBox = false;
+            // サイズ変更不可
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            // MaskTextBox 割り込み登録
+            maskedTextBox1.KeyDown += new KeyEventHandler(this.MaskTextBox_KeyDown);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+            // 配置：中央
+            maskedTextBox1.TextAlign = HorizontalAlignment.Center;
+
+            // マスク設定
+            //maskedTextBox1.Mask = ">CC CC CC CC CC CC CC CC CC CC CC CC CC CC CC CC";
+            maskedTextBox1.Mask = ">AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA";
+            maskedTextBox1.Text = "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00";
+
+        }
+
+        /// <summary>
+        /// 入力文字制御(HEX)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MaskTextBox_KeyDown(object sender, KeyEventArgs e) {
+
+            bool keyPressCk = true;
+
+            if ( ( 'A' <= e.KeyValue && e.KeyValue <= 'F' ) // A～F
+             || ( '0' <= e.KeyValue && e.KeyValue <= '9' ) // 0～9 (キーボード)
+             || ( ( int ) Keys.NumPad0 <= e.KeyValue && e.KeyValue <= ( int ) Keys.NumPad9 ) // 1～9(テンキー) 
+             || ( ( int ) Keys.Left == e.KeyValue || ( int ) Keys.Right == e.KeyValue ) ) // ←→
+            {
+                keyPressCk = false;
+            }
+
+            // [KeyDown]後の[KeyPress]イベント
+            e.SuppressKeyPress = keyPressCk; // true = 無効 / false = 許可
+        }
+    }
+}
